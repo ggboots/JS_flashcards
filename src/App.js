0276927e0d909dd0d28a279f2ff507851a9072f0
George Boots <ggboots.99@gmail.com>
2022-10-14 23:08:32 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Cards from "./coding_cards/Cards.js"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App backdrop">
+        <Cards />
+        <div>
+          <button>Draw</button>
+          <button>Flip</button>
+        </div>
     </div>
   );
 }
---
+++ src/coding_cards/Cards.js
@@ -0,0 +1,12 @@
+import React from "react";
+import anImage from "./card_assests/c1_front.jpg"
+
+function Cards(){
+    return(
+        <div className="cards">
+            <image src={anImage}/>
+        </div>
+    )
+}
+
+export default Cards
